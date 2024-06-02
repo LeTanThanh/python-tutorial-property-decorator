4c912024ddc4eb41c7de74153d9f0338688a7250
TanThanhLe <tanthanhcr95@gmail.com>
2024-06-03 00:12:25 +0700

    Setter decorators

--- main.py
+++ main.py
@@ -38,3 +38,36 @@ Therefore, you can use the @property decorator to decorator the age() method as 
 
 So by using the @property decorator, you can simplify the property definition for a class.
 """
+
+## Setter decorators
+
+"""
+The following adds a setter method (set_age) to assign a value to _age attribute to the Person class:
+
+To assign the set_age to the fset of the age property object, you cann the setter() method of the age property object like the following:
+
+The setter() method accepts a callable and returns another callable (a property object).
+Therefore, you can use the decorator @age.setter for the set_age() method like this:
+
+Now, you can change the set_age() method to the age() method and use the age property in the __init__() method:
+
+To summarize, you can use decorators to create a property using the following pattern:
+"""
+
+class MyClass:
+  def __init__(self, attr) -> None:
+    self.prop = attr
+
+  @property
+  def prop(self):
+    return self.__attr
+
+  @prop.setter
+  def prop(self, value):
+    self.__attr = value
+
+"""
+In this pattern, the __attr is the private attribute and prop is the property name.
+
+The following example uses the @property decorators to create the name and age properties in the Person class:
+"""
--- person.py
+++ person.py
@@ -1,8 +1,26 @@
 class Person:
   def __init__(self, name, age) -> None:
     self.name = name
-    self._age = age
+    self.age = age
 
   @property
   def age(self):
     return self._age
+
+  @age.setter
+  def age(self, value):
+    if value <= 0:
+      raise ValueError("The age must be positive")
+
+    self._age = value
+
+  @property
+  def name(self):
+    return self._name
+
+  @name.setter
+  def name(self, value):
+    if value.strip() == "":
+      raise ValueError("The name cannoy be empty")
+
+    self._name = value
